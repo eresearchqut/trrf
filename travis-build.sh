0f7f20dc175c4c59297fbbf78e942e2f1f483f2b
Tamas Szabo <szabtam@gmail.com>
2019-03-11 15:19:08 +1000

    Rewriting docker build chain.
      - removed references to muccg docker and develop.sh references
      - updated docker-compose files to latest version (3.7)
      - documented commands to use instead of develop.sh commands in
    travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 #
 # Development build and tests
@@ -8,15 +8,23 @@ set -e
 
 # ccg-composer runs as this UID, and needs to be able to
 # create output directories within it
-mkdir -p data/
-sudo chown 1000:1000 data/
+# mkdir -p data/
+# sudo chown 1000:1000 data/
 
-./develop.sh build base
-./develop.sh build builder
-./develop.sh build node
-./develop.sh build dev
-./develop.sh check-migrations
-./develop.sh run-unittests
-./develop.sh aloe teststack
-./develop.sh run build lint
-./develop.sh run "" node lint
+# ./develop.sh build base
+# ./develop.sh build builder
+# ./develop.sh build node
+# ./develop.sh build dev
+docker-compose -f dev.yml build
+
+# ./develop.sh check-migrations
+docker-compose -f dev.yml run runserver django-admin makemigrations --dry-run --noinput --check
+
+#./develop.sh run-unittests
+#./develop.sh aloe teststack
+
+#./develop.sh run build lint
+docker run -it --rm --volume $(pwd):/apps alpine/flake8 .
+#./develop.sh run "" node lint
+docker-compose -f dev.yml run --rm node lint
+
