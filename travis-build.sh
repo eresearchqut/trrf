17e50b948b17d369779913d533aef904e04c3cc6
Tamas Szabo <szabtam@gmail.com>
2019-03-12 17:18:32 +1000

    Rewriting docker build chain, adds teststack, aloe.

--- travis-build.sh
+++ travis-build.sh
@@ -18,13 +18,17 @@ set -e
 docker-compose -f dev.yml build
 
 # ./develop.sh check-migrations
-docker-compose -f dev.yml run runserver django-admin makemigrations --dry-run --noinput --check
+docker-compose -f dev.yml run --rm runserver django-admin makemigrations --dry-run --noinput --check
 
 #./develop.sh run-unittests
+docker-compose -f test.yml run runservertest runtests
+
+
 #./develop.sh aloe teststack
+./run-aloe.sh
 
 #./develop.sh run build lint
-docker run -it --rm --volume $(pwd):/apps alpine/flake8 .
+docker run --rm --volume $(pwd):/apps alpine/flake8 .
 #./develop.sh run "" node lint
 docker-compose -f dev.yml run --rm node lint
 
