4ad637838bc6333a166b28b2b384cc1825772dbe
Tamas Szabo <szabtam@gmail.com>
2019-03-18 15:50:40 +1000

    Changed default project title to TRRF.

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -485,7 +485,7 @@ REST_FRAMEWORK = {
 # setup for SYSTEM_ROLE
 SYSTEM_ROLE = SystemRoles.from_value(env.get("SYSTEM_ROLE", "NORMAL"))
 
-PROJECT_TITLE = env.get("project_title", "Rare Disease Registry Framework")
+PROJECT_TITLE = env.get("project_title", "Trial Ready Registry Framework")
 PROJECT_TITLE_LINK = "admin:index" if SYSTEM_ROLE is SystemRoles.CIC_PROMS else "patientslisting"
 
 # project logo and link
