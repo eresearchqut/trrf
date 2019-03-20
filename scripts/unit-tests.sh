70eecce62f5f8b537235f414dfe8d1d3ab6ff5e9
Tamas Szabo <szabtam@gmail.com>
2019-03-20 14:29:03 +1000

    Stopping the teststack instead of removing it

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -2,4 +2,4 @@
 
 docker-compose -f docker-compose-teststack-base.yml -f docker-compose-teststack-dev.yml run serverundertest runtests
 
-docker-compose -f docker-compose-teststack-base.yml -f docker-compose-teststack-dev.yml down
+docker-compose -f docker-compose-teststack-base.yml -f docker-compose-teststack-dev.yml stop
