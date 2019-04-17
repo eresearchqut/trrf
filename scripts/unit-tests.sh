905b3d4190cddde12777d1faf4075af0d319927b
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2019-04-17 17:06:32 +0800

    Make unit-test.sh return false if unit tests fail.

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 docker-compose -f docker-compose-teststack-base.yml -f docker-compose-teststack-dev.yml run serverundertest runtests
+RESULT=$?
 
 docker-compose -f docker-compose-teststack-base.yml -f docker-compose-teststack-dev.yml stop
+
+exit $RESULT
