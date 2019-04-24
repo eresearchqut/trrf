fe18ee6f0e57e4d32921f09dc4324ec870dec108
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2019-04-24 14:32:19 +0800

    Put all env variables in .env file.

--- scripts/codedeploy/after_install.sh
+++ scripts/codedeploy/after_install.sh
@@ -3,17 +3,17 @@ set -x
 mkdir -p /home/ec2-user/efs/trrf/data/log
 chown -R ec2-user:ec2-user /home/ec2-user/efs/trrf
 cd /home/ec2-user/efs/trrf
-source .env
 
 # TODO
 # export UWSGI_IMAGE=$IMAGE_REPO:$APPLICATION_VERSION
-export UWSGI_IMAGE=126579111836.dkr.ecr.ap-southeast-2.amazonaws.com/eresearchqut/trrf2:latest
-export TRRF_VERSION=1.0.0
-export DJANGO_FIXTURES=default
+echo "UWSGI_IMAGE=126579111836.dkr.ecr.ap-southeast-2.amazonaws.com/eresearchqut/trrf2:latest" >> .env
+echo "TRRF_VERSION=1.0.0" >> .env
+echo "DJANGO_FIXTURES=default" >> .env
 
 # TODO update path and make it work based on the environment and project name
-aws --profile ssm get-parameters-by-path --path /app/eresearchqut/trrf --with-decryption | jq '.Parameters | .[] | [(.Name | ltrimstr("/app/eresearchqut/trrf/")), .Value] | join("=")' -r > .env_from_ssm
+aws --profile ssm get-parameters-by-path --path /app/eresearchqut/trrf --with-decryption | jq '.Parameters | .[] | [(.Name | ltrimstr("/app/eresearchqut/trrf/")), .Value] | join("=")' -r >> .env
 
-source .env_from_ssm
+source .env
+# source .env_from_ssm
 
 
