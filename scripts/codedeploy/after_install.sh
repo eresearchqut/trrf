7383e918e9784833dbf111990d3382c09390b31c
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2019-04-25 09:03:22 +0800

    Renaming of DG and App in CodeDeploy.

--- scripts/codedeploy/after_install.sh
+++ scripts/codedeploy/after_install.sh
@@ -6,14 +6,22 @@ cd /home/ec2-user/efs/trrf
 
 # TODO
 # export UWSGI_IMAGE=$IMAGE_REPO:$APPLICATION_VERSION
+
 echo "UWSGI_IMAGE=126579111836.dkr.ecr.ap-southeast-2.amazonaws.com/eresearchqut/trrf2:latest" >> .env
-echo "TRRF_VERSION=1.0.0" >> .env
-echo "DJANGO_FIXTURES=default" >> .env
+# echo "TRRF_VERSION=1.0.0" >> .env
+# echo "DJANGO_FIXTURES=default" >> .env
+
+echo UWSGI_IMAGE=`aws ecr describe-repositories --repository-name $APPLICATION_NAME | jq '.repositories | .[0] | .repositoryUri'` >> .env
+
+
+export SSM_PATH=/${DEPLOYMENT_GROUP_NAME}/${APPLICATION_NAME}/
+
+aws --profile r4 ssm get-parameters-by-path --path ${SSM_PATH} --with-decryption | jq ".Parameters | .[] | [(.Name | ltrimstr(\"$SSM_PATH\")), .Value] | join(\"=\")" -r
 
 # TODO update path and make it work based on the environment and project name
-aws --profile ssm get-parameters-by-path --path /app/eresearchqut/trrf --with-decryption | jq '.Parameters | .[] | [(.Name | ltrimstr("/app/eresearchqut/trrf/")), .Value] | join("=")' -r >> .env
+#aws ssm get-parameters-by-path --path /app/eresearchqut/trrf --with-decryption | jq '.Parameters | .[] | [(.Name | ltrimstr("/app/eresearchqut/trrf/")), .Value] | join("=")' -r >> .env
 
-source .env
+# source .env
 # source .env_from_ssm
 
 
