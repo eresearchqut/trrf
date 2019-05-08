e455269cc7334143cb71f4214e6b9c2e3dceeb34
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2019-05-08 16:36:05 +0300

    after_install.sh script set default region

--- scripts/codedeploy/start_service.sh
+++ scripts/codedeploy/start_service.sh
@@ -2,6 +2,7 @@
 set -xe
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
 
+export AWS_DEFAULT_REGION=ap-southeast-2
 export UWSGI_IMAGE=`aws ecr describe-repositories --repository-name $APPLICATION_NAME | jq '.repositories | .[0] | .repositoryUri' -r`
 
 docker pull $UWSGI_IMAGE
