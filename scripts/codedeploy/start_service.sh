bdcef6291eeba537091e859447488b8ae77f70ae
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2019-05-08 16:28:43 +0300

    Fix in previous change - fetch proper docker image

--- scripts/codedeploy/start_service.sh
+++ scripts/codedeploy/start_service.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -xe
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
+
+export UWSGI_IMAGE=`aws ecr describe-repositories --repository-name $APPLICATION_NAME | jq '.repositories | .[0] | .repositoryUri' -r`
+
 docker pull $UWSGI_IMAGE
+
 cd /home/ec2-user/efs/trrf
 docker-compose -f docker-compose-prod.yml up -d
