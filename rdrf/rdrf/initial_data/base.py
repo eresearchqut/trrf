64295b438f0a0752a5e807d48bb19015e9fc517c
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2019-05-30 09:29:21 +0300

    New configuration params TRRF_SITE_DOMAIN and TRRF_SITE_NAME
    https://eresearchqut.atlassian.net/browse/TRRFFDA-216

--- rdrf/rdrf/initial_data/base.py
+++ rdrf/rdrf/initial_data/base.py
@@ -3,6 +3,23 @@
 This dataset is always installed when init is used, so add only required
 datasets to it."""
 
+import os
+
+from django.contrib.sites.models import Site
+
 
 def load_data(**kwargs):
-    pass
+    set_up_site()
+
+
+def set_up_site():
+    if not 'TRRF_SITE_DOMAIN' in os.environ:
+        return
+
+    domain = os.environ['TRRF_SITE_DOMAIN']
+    name = os.environ.get('TRRF_SITE_NAME', os.environ.get('PROJECT_TITLE', domain))
+
+    site = Site.objects.get_current()
+    site.domain = domain
+    site.name = name
+    site.save()
