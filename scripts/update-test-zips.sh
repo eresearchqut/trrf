ebe9541e092f9c4b93c19e1583fe8af358027bc0
Tamas Szabo <szabtam@gmail.com>
2019-06-12 08:35:27 +0300

    update-test-zips.sh fix: wait for migrations to complete

--- scripts/update-test-zips.sh
+++ scripts/update-test-zips.sh
@@ -1,16 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-alias teststack="docker-compose -f docker-compose-teststack-base.yml -f docker-compose-teststack-dev.yml"
-alias teststack-exec-django="teststack exec serverundertest /docker-entrypoint.sh django-admin.py"
+teststack() {
+  docker-compose -f docker-compose-teststack-base.yml -f docker-compose-teststack-dev.yml "$@"
+}
+
+teststack-exec-django() {
+  teststack exec serverundertest /docker-entrypoint.sh django-admin.py "$@"
+}
+
+
+wait_for_server() {
+    while ! curl -Is http://localhost:$1 > /dev/null; do
+        echo "$(date) - waiting to connect $1"
+        sleep 5
+    done
+    echo "$(date) - connected to $1"
+}
 
 export_zip() {
     teststack stop
     teststack rm --force
     teststack up -d
 
+    PORT=$(teststack port serverundertest 8000 | cut -d':' -f2)
+    wait_for_server $PORT
+
     set +e
 
-    teststack-exec-django migrate
     teststack-exec-django import "${1}" --force
     teststack-exec-django export registry --filename "${1}" --registry-code "${2}"
 
