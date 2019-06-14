39904d4485d7ecbf793a341c67973c2459f3fcb8
Alin Treznai <alin.treznai.gemini@gmail.com>
2019-06-14 10:20:56 +0300

    Code cleanup

--- rdrf/registry/patients/patient_view.py
+++ rdrf/registry/patients/patient_view.py
@@ -4,11 +4,12 @@ from django.views.generic.base import View
 
 from rdrf.models.definition.models import Registry
 
+from .mixins import LoginRequiredMixin
 
-class PatientView(View):
+
+class PatientView(View, LoginRequiredMixin):
 
     def get(self, request, registry_code):
-        if request.user.is_authenticated:
-            _ = get_object_or_404(Registry,code=registry_code)
-            patient = request.user.user_object.first()
-            return redirect(reverse("patient_edit", args=[registry_code, patient.id, ]))
+        _ = get_object_or_404(Registry, code=registry_code)
+        patient = request.user.user_object.first()
+        return redirect(reverse("patient_edit", args=[registry_code, patient.id, ]))
