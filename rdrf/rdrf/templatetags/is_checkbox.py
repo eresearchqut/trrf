cb3b7d1bc59dc8a153abee7ec43f8d7cc3e0051f
Alin Treznai <alin.treznai.gemini@gmail.com>
2019-06-18 07:44:25 +0300

    Updates based on code review

--- rdrf/rdrf/templatetags/is_checkbox.py
+++ rdrf/rdrf/templatetags/is_checkbox.py
@@ -1,6 +1,4 @@
 from django import template
-from django.forms import CheckboxInput
-from django.forms.widgets import CheckboxSelectMultiple
 
 register = template.Library()
 
@@ -13,8 +11,6 @@ def is_checkbox(element):
     """
     if hasattr(element, "field"):
         field = element.field
+        return field.widget.input_type == 'checkbox'
 
-    field_class_name = field.widget.__class__.__name__
-    checkbox_class_names = (CheckboxInput().__class__.__name__, CheckboxSelectMultiple().__class__.__name__, )
-
-    return field_class_name in checkbox_class_names
+    return False
