8d1a05fd357b7c92337582b78feee20ca00580a8
Alin Treznai <alin.treznai.gemini@gmail.com>
2019-08-06 10:09:08 +0300

    Tests cleanup

--- rdrf/rdrf/testing/unit/quicklinks_tests.py
+++ rdrf/rdrf/testing/unit/quicklinks_tests.py
@@ -28,6 +28,14 @@ class QuickLinksTests(RDRFTestCase):
         self.user.working_groups.add(self.wg)
         super().setUp()
 
+    def contains_all_values(self, values_dict, container):
+        for value in values_dict.values():
+            self.assertIn(value, container)
+
+    def does_not_contains_values(self, values_dict, container):
+        for value in values_dict.values():
+            self.assertIn(value, container)
+
     @override_settings(SYSTEM_ROLE=SystemRoles.CIC_PROMS)
     def test_cic_proms_menus(self):
         ql = QuickLinks([self.registry])
@@ -36,61 +44,47 @@ class QuickLinksTests(RDRFTestCase):
         sl = ql.settings_links()
         self.assertEqual(len(sl), 0)
         al = ql.admin_page_links()
-        for value in PromsLinks.CIC.values():
-            self.assertIn(value, al)
+        self.contains_all_values(PromsLinks.CIC, al)
 
     @override_settings(SYSTEM_ROLE=SystemRoles.CIC_PROMS)
     @override_settings(DESIGN_MODE=True)
     def test_cic_proms_menus_design_mode(self):
         ql = QuickLinks([self.registry])
         al = ql.admin_page_links()
-        for value in PromsLinks.CIC.values():
-            self.assertIn(value, al)
-        for value in PromsLinks.REGISTRY_DESIGN.values():
-            self.assertIn(value, al)
+        self.contains_all_values(PromsLinks.CIC, al)
+        self.contains_all_values(PromsLinks.REGISTRY_DESIGN, al)
 
     @override_settings(SYSTEM_ROLE=SystemRoles.NORMAL)
     def test_regular_menus(self):
         ql = QuickLinks([self.registry])
         ml = ql.menu_links(["Working Group Curators"])
         self.assertNotEqual(len(ml), 0)
-        for value in RegularLinks.DATA_ENTRY.values():
-            self.assertIn(value, ml)
+        self.contains_all_values(RegularLinks.DATA_ENTRY, ml)
         sl = ql.settings_links()
         self.assertNotEqual(len(sl), 0)
-        for value in RegularLinks.AUDITING.values():
-            self.assertIn(value, sl)
+        self.contains_all_values(RegularLinks.AUDITING, sl)
         al = ql.admin_page_links()
-        for value in PromsLinks.CIC.values():
-            self.assertNotIn(value, al)
-        for value in RegularLinks.EMAIL.values():
-            self.assertIn(value, al)
+        self.does_not_contains_values(PromsLinks.CIC, al)
+        self.contains_all_values(RegularLinks.EMAIL, al)
 
     @override_settings(SYSTEM_ROLE=SystemRoles.NORMAL)
     @override_settings(DESIGN_MODE=True)
     def test_regular_menus_design_mode(self):
         ql = QuickLinks([self.registry])
         al = ql.admin_page_links()
-        for value in PromsLinks.CIC.values():
-            self.assertNotIn(value, al)
-        for value in PromsLinks.REGISTRY_DESIGN.values():
-            self.assertIn(value, al)
-        for value in RegularLinks.EMAIL.values():
-            self.assertIn(value, al)
+        self.does_not_contains_values(PromsLinks.CIC, al)
+        self.contains_all_values(PromsLinks.REGISTRY_DESIGN, al)
+        self.contains_all_values(RegularLinks.EMAIL, al)
 
     @override_settings(SYSTEM_ROLE=SystemRoles.CIC_DEV)
     def test_cic_non_proms_menus(self):
         ql = QuickLinks([self.registry])
         ml = ql.menu_links(["Working Group Curators"])
         self.assertNotEqual(len(ml), 0)
-        for value in RegularLinks.DATA_ENTRY.values():
-            self.assertIn(value, ml)
+        self.contains_all_values(RegularLinks.DATA_ENTRY, ml)
         sl = ql.settings_links()
         self.assertNotEqual(len(sl), 0)
-        for value in RegularLinks.AUDITING.values():
-            self.assertIn(value, sl)
+        self.contains_all_values(RegularLinks.AUDITING, sl)
         al = ql.admin_page_links()
-        for value in PromsLinks.CIC.values():
-            self.assertIn(value, al)
-        for value in RegularLinks.EMAIL.values():
-            self.assertIn(value, al)
+        self.contains_all_values(RegularLinks.CIC, al)
+        self.contains_all_values(RegularLinks.EMAIL, al)
