14d38f648d78566cdb6f3f0069cc3defb7eeebaf
Tamas Szabo <szabtam@gmail.com>
2019-09-13 13:07:28 +0300

    Fix TRRFFDA-374 New banner with Patient Info flows over alert messages

--- rdrf/rdrf/static/js/rdrf.js
+++ rdrf/rdrf/static/js/rdrf.js
@@ -5,3 +5,15 @@ function hide_empty_menu() {
         $(".dropdown-menu-button").hide();
     }
 }
+
+// Some pages can have larger banners than others, adjusting the top padding of the main content
+// so that the banner doesn't overflow
+function adjustContentTopPadding() {
+    var navbarHeight = $(".navbar").height();
+    var bannerHeight = $(".banner").height();
+    if (navbarHeight == null || bannerHeight == null) {
+        // Can't find navbar or banner, better not do anything
+        return
+    }
+    $("#content").css({"padding-top": navbarHeight + bannerHeight});
+}
