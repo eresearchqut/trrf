b79e2d8f2e9485f617c8e631a120516eb4364f3f
Chris Williams <chris.j.williams@qut.edu.au>
2019-10-02 13:16:10 +1000

    add nginx container to start script

--- scripts/codedeploy/start_service.sh
+++ scripts/codedeploy/start_service.sh
@@ -3,12 +3,14 @@ set -xe
 
 export AWS_DEFAULT_REGION=ap-southeast-2
 export ECR_ACCOUNT_ID=$(aws ssm get-parameter --name ECR-ACCOUNT-ID | jq -r .Parameter.Value)
+export TRRF_NGINX_PROXY=$(aws ssm get-parameter --name TRRF-NGINX-PROXY | jq -r .Parameter.Value)
 export ECR_URL="${ECR_ACCOUNT_ID}.dkr.ecr.ap-southeast-2.amazonaws.com"
 
 $(aws ecr get-login --no-include-email --registry-ids ${ECR_ACCOUNT_ID})
 
 # TODO TRRF_VERSION should probably appended to this here
 export UWSGI_IMAGE="${ECR_URL}/${APPLICATION_NAME}"
+export NGINX_DOCKER_IMAGE="${ECR_URL}/${TRRF_NGINX_PROXY}"
 
 docker pull $UWSGI_IMAGE
 
