b1d6bee2203e91facbc2e89d6b24caa24fff7df1
Alin Treznai <alin.treznai.gemini@gmail.com>
2019-10-02 10:42:30 +0300

    Added first visit field for patient serializer for the demo

--- rdrf/rdrf/services/rest/serializers.py
+++ rdrf/rdrf/services/rest/serializers.py
@@ -55,6 +55,32 @@ class PatientSerializer(serializers.HyperlinkedModelSerializer):
     age = serializers.IntegerField(read_only=True)
     url = PatientHyperlinkId(read_only=True, source='*')
     user = CustomUserSerializer()
+    first_visit = serializers.SerializerMethodField()
+
+    def get_first_visit(self, obj):
+        registries = obj.rdrf_registry.all()
+        target_forms = [
+            form_model
+            for registry_model in registries
+            for form_model in registry_model.forms
+            if form_model.name.lower() == 'firstvisit'
+        ]
+        if not target_forms:
+            return None
+        form_model = target_forms[0]
+        for section_model in form_model.section_models:
+            for cde_model in section_model.cde_models:
+                if cde_model.name.lower() == 'visit date':
+                    try:
+                        return obj.get_form_value(
+                            form_model.registry.code,
+                            form_model.name,
+                            section_model.code,
+                            cde_model.code,
+                            section_model.allow_multiple
+                        )
+                    except KeyError:
+                        return None
 
     class Meta:
         model = Patient
