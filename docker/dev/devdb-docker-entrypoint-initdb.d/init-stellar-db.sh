ec5ee8d417a353b2a7caad011a7826511649e045
Tamas Szabo <szabtam@gmail.com>
2019-10-24 11:02:43 +1030

    TRRFFDA-401: getting rid of end2end test errors
    Fixes https://eresearchqut.atlassian.net/browse/TRRFFDA-401

---
+++ docker/dev/devdb-docker-entrypoint-initdb.d/init-stellar-db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE stellar_data ENCODING 'utf8' TEMPLATE template1;
+EOSQL
