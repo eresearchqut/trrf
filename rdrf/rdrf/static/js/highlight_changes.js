bc8bea5247f8864fb6bec94cbbfc7e7de8a8fab2
Tamas Szabo <szabtam@gmail.com>
2019-12-03 14:37:00 +1000

    Bugfix: checkbox highlighting on changes (#TRRFFDA-450)

--- rdrf/rdrf/static/js/highlight_changes.js
+++ rdrf/rdrf/static/js/highlight_changes.js
@@ -12,17 +12,16 @@ $(document).ready(function() {
   var old_border;
   var old_background_color;
 
-  var old_radio_border;
-  var old_radio_background_color;
-
   // Save defaults in each DOM element's pre data and append transitions
   $('#main-form *').filter(monitored_fields).each(function() {
-    if (!$(this).is(':radio')) {
-      $(this).data('pre', $(this).val()+'');
-      $(this).css("transition", transition);
-    } else {
+    if ($(this).is(':radio')) {
       $(this).parent().parent().data('pre', $(this).parent().parent().find(':checked').val());
       $(this).parent().parent().css("transition", transition);
+    } else if ($(this).is(':checkbox')) {
+      $(this).data('pre', $(this).is(":checked") ? $(this).val() : '');
+    } else {
+      $(this).data('pre', $(this).val()+'');
+      $(this).css("transition", transition);
     }
   });
 
@@ -36,35 +35,39 @@ $(document).ready(function() {
     if (!old_background_color) {
       old_background_color = $('#main-form *').filter('input').first().css("background-color");
     }
-    if (!old_radio_border) {
-      old_radio_border = $('#main-form *').filter(':radio').first().parent().parent().css("border");
-    }
-    if (!old_radio_background_color) {
-      old_radio_background_color = $('#main-form *').filter(':radio').first().parent().parent().css("background-color");
-    }
 
     $('#main-form *').filter(monitored_fields).each(function() {
       if ($(this).hasClass('timepicki-input')) {
         return;
       }
-      if (!$(this).is(':radio')) {
-        if ($(this).val() != $(this).data('pre')) {
-          $(this).css("border", new_border);
-          $(this).css("background-color", new_background_color);
-        } else {
-          $(this).css("border", old_border);
-          $(this).css("background-color", old_background_color);
-        }
-      } else {
-        if ($(this).parent().parent().find(':checked').val() != $(this).parent().parent().data('pre')) {
-          $(this).parent().parent().css("border", new_border);
-          $(this).parent().parent().css("background-color", new_background_color);
-        } else {
-          $(this).parent().parent().css("border", old_radio_border);
-          $(this).parent().parent().css("background-color", old_radio_background_color);
-        }
 
+      function didChange(el) {
+        var pre, curVal;
+        if (el.is(':radio')) {
+          pre = el.parent().parent().data('pre');
+          curVal = el.parent().parent().find(':checked').val();
+        } else if (el.is(':checkbox')) {
+          pre = el.data('pre');
+          curVal = el.is(":checked") ? el.val() : '';
+        } else {
+          pre = el.data('pre');
+          curVal = el.val();
+        }
+        return pre !== curVal;
       }
+
+      var didValueChange = didChange($(this));
+
+      var border = didValueChange ? new_border : old_border;
+      var background_color = didValueChange ? new_background_color : old_background_color;
+
+      var elementToHighlight = ($(this).is(':radio') || $(this).is(':checkbox')) ? $(this).parent().parent() : $(this);
+
+      if (!($(this).is(':radio') || $(this).is(':checkbox'))) {
+        // No border changes for radio boxes and checkboxes
+        elementToHighlight.css('border', border);
+      }
+      elementToHighlight.css('background-color', background_color);
     });
   });
 });
