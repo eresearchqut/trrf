da494ca3ff83f1b7df21dc60f8fff09c7ee32a04
Adam Smith <smitha50@qut.edu.au>
2019-12-13 10:29:15 +1000

    Change default email from address

--- rdrf/rdrf/models/definition/models.py
+++ rdrf/rdrf/models/definition/models.py
@@ -1532,7 +1532,7 @@ class EmailNotification(models.Model):
 
     description = models.CharField(max_length=100, choices=EMAIL_NOTIFICATIONS)
     registry = models.ForeignKey(Registry, on_delete=models.CASCADE)
-    email_from = models.EmailField(default='No Reply <no-reply@mg.ccgapps.com.au>')
+    email_from = models.EmailField(default='no-reply@trrf.registryframework.net')
     recipient = models.CharField(max_length=100, null=True, blank=True)
     group_recipient = models.ForeignKey(Group, null=True, blank=True, on_delete=models.SET_NULL)
     email_templates = models.ManyToManyField(EmailTemplate)
