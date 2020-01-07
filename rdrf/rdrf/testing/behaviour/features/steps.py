97e5578c8ea3d2b9e898fb376c6747492fb02d34
Tamas Szabo <szabtam@gmail.com>
2020-01-07 21:10:42 +0200

    E2E tests: adding another wait for first section

--- rdrf/rdrf/testing/behaviour/features/steps.py
+++ rdrf/rdrf/testing/behaviour/features/steps.py
@@ -258,6 +258,8 @@ def enter_cde_on_form_multisection(step, cde_value, form, section, cde, item):
 
     location_is(step, form)  # sanity check
 
+    utils.wait_for_first_section()
+
     form_block = world.browser.find_element_by_id("main-form")
     section_div_heading = form_block.find_element_by_xpath(
         ".//div[@class='panel-heading'][contains(., '%s')]" % section)
