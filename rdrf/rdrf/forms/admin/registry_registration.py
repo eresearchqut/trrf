d04d322b5223d9acae95d3a697e269cecb8eaabc
Adam Smith <smitha50@qut.edu.au>
2020-01-16 13:29:06 +1000

    Additional translation

--- rdrf/rdrf/forms/admin/registry_registration.py
+++ rdrf/rdrf/forms/admin/registry_registration.py
@@ -20,9 +20,9 @@ class RegistrationAdminForm(Form):
         label=_('Create a new email notification for patients upon registration'),
     )
 
-    from_address = CharField(required=False, widget=BootstrapTextInput)
+    from_address = CharField(required=False, widget=BootstrapTextInput, label=_("From address"))
 
-    language = ChoiceField(required=False, choices=settings.ALL_LANGUAGES, widget=BootstrapSelectInput)
-    description = CharField(required=False, widget=BootstrapTextInput)
-    subject = CharField(required=False, max_length=50, widget=BootstrapTextInput)
-    body = CharField(required=False, widget=BootstrapTextAreaInput)
+    language = ChoiceField(required=False, choices=settings.ALL_LANGUAGES, widget=BootstrapSelectInput, label=_("Language"))
+    description = CharField(required=False, widget=BootstrapTextInput, label=_("Description"))
+    subject = CharField(required=False, max_length=50, widget=BootstrapTextInput, label=_("Subject"))
+    body = CharField(required=False, widget=BootstrapTextAreaInput, label=_("Body"))
