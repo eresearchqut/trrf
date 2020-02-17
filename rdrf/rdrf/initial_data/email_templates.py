9a0c50bf1bdeef55597d1a88d828afad82dd2231
Adam Smith <smitha50@qut.edu.au>
2020-02-17 13:41:45 +1000

    Move email template fixtures to their own file

---
+++ rdrf/rdrf/initial_data/email_templates.py
@@ -0,0 +1,18 @@
+from django.template.loader import get_template
+
+from rdrf.events.events import EventType
+from rdrf.models.definition.models import EmailTemplate
+
+
+def load_data(**kwargs):
+    load_default_patient_registration_template()
+
+
+def load_default_patient_registration_template():
+    EmailTemplate.objects.get_or_create(
+        language="en",
+        description="trrf_default_patient_registration_en",
+        subject="Welcome to the registry",
+        body=get_template("registration/default_patient_registration_en.html").template.source,
+        default_for_notification=EventType.NEW_PATIENT
+    )
--- rdrf/rdrf/initial_data/reference_data.py
+++ rdrf/rdrf/initial_data/reference_data.py
@@ -3,18 +3,13 @@
 Includes:
     - Address Types
 """
-from django.template.loader import get_template
-
-from rdrf.events.events import EventType
-from rdrf.models.definition.models import EmailTemplate
 from registry.patients.models import AddressType
 
-deps = ['patient_stage']
+deps = ['patient_stage', 'email_templates']
 
 
 def load_data(**kwargs):
     load_address_types()
-    load_default_notifications()
 
 
 def load_address_types():
@@ -22,11 +17,3 @@ def load_address_types():
     AddressType.objects.get_or_create(type="Postal", description="Postal Address")
 
 
-def load_default_notifications():
-    EmailTemplate.objects.get_or_create(
-        language="en",
-        description="trrf_default_patient_registration_en",
-        subject="Welcome to the registry",
-        body=get_template("registration/default_patient_registration_en.html").template.source,
-        default_for_notification=EventType.NEW_PATIENT
-    )
