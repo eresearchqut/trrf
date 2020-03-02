3c03035515c45dfb33520bc69f433ba2c388961d
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-03-02 10:13:30 +0200

    Refactor widget settings

--- rdrf/rdrf/forms/widgets/settings_widgets.py
+++ rdrf/rdrf/forms/widgets/settings_widgets.py
@@ -1,242 +1,151 @@
 import json
 
 from django.forms import Widget
+from django.forms.renderers import get_default_renderer
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
 
 from .widgets import TimeWidget
 
 
-class SliderWidgetSettings(Widget):
+class JSONWidget(Widget):
 
-    @staticmethod
-    def get_allowed_fields():
-        return {'min', 'max', 'left_label', 'right_label', 'step'}
+    def get_allowed_fields(self):
+        pass
 
-    @staticmethod
-    def generate_input(name, title, parsed, info=None):
-        value = parsed.get(name, '')
-        input_str = f'<input type="text" name="{name}" id="{name}" value="{value}" onchange="saveJSON()">'
-        help_text = f'<div class="help">{info}</div>' if info else ''
-        return f"""
-            <div>
-                <label for="{name}">{title}</label>
-                {input_str}
-                {help_text}
-            </div>"""
-
-    def generate_inputs(self, parsed):
-        rows = [
-            self.generate_input('min', _('Min value'), parsed, _("leave empty if you want to use the CDE's min value")),
-            self.generate_input('max', _('Max value'), parsed, _("leave empty if you want to use the CDE's max value")),
-            self.generate_input('left_label', _('Left label'), parsed),
-            self.generate_input('right_label', _('Right label'), parsed),
-            self.generate_input('step', _('Step'), parsed),
-        ]
-        return "<br/>".join(rows)
-
-    def render(self, name, value, attrs=None, renderer=None):
-        parsed = {}
+    def parse_value(self, value):
+        self.parsed = {}
         try:
-            parsed = json.loads(value)
+            self.parsed = json.loads(value)
+            self.parsed = {k: v for k, v in self.parsed.items() if k in self.get_allowed_fields()}
         except Exception:
             pass
 
-        html = """
-             <div style="display:inline-grid" id="id_{name}">
-                {inputs}
-                <input type="hidden" name="{name}" value='{value}'/>
-             </div>""".format(inputs=self.generate_inputs(parsed), name=name, value=value)
-        javascript = """
-            function saveJSON() {
-                var inputs = $('#id_%s input[type!=hidden]');
-                var obj = {};
-                for (var i = 0; i < inputs.length; i++) {
-                    if (inputs[i].value !='' && inputs[i].value.trim() != '') {
-                        obj[inputs[i].name] = inputs[i].value;
-                    }
-                }
-                $("input[name='%s']").val(JSON.stringify(obj));
-            }
-            saveJSON();
-        """ % (name, name)
-        return mark_safe(f"""
-            {html}
-            <script>
-                {javascript}
-            </script>""")
+    def _update_input(self, name, title, info=None, onchange=None):
+        value = self.parsed.get(name, '')
+        self.parsed[name] = {
+            'name': name,
+            'value': value,
+            'title': title,
+            'info': info,
+            'onchange': onchange or 'saveJSON()'
+        }
+
+    def generate_text_input(self, name, title, **kwargs):
+        self._update_input(name, title, kwargs.get('info'), kwargs.get('onchange'))
+        self.parsed[name]['input_type'] = 'text'
+
+    def generate_checkbox_input(self, name, title, **kwargs):
+        self._update_input(name, title, kwargs.get('info'), kwargs.get('onchange'))
+        self.parsed[name]['input_type'] = 'checkbox'
+        self.parsed[name]['checked'] = kwargs.get('checked')
+
+    def generate_select_input(self, name, title, **kwargs):
+        self._update_input(name, title, kwargs.get('info'), kwargs.get('onchange'))
+        self.parsed[name]['input_type'] = 'select'
+        self.parsed[name]['options'] = kwargs.get('options', [])
+
+    def generate_inputs(self):
+        pass
+
+    def render(self, name, value, attrs=None, renderer=None):
+        self.parse_value(value)
+        self.generate_inputs()
+        return self.render_widget(name, value, renderer)
+
+    def render_widget(self, name, value, renderer=None, javascript=None):
+        if not renderer:
+            renderer = get_default_renderer()
+        context = {
+            "settings": self.parsed,
+            "name": name,
+            "value": value,
+        }
+        if javascript:
+            context.update({"extra_js": mark_safe(javascript)})
+
+        return renderer.render("widgets/widget_settings.html", context)
 
 
-class TimeWidgetSettings(Widget):
+class SliderWidgetSettings(JSONWidget):
 
-    @staticmethod
-    def get_allowed_fields():
+    def get_allowed_fields(self):
+        return {'min', 'max', 'left_label', 'right_label', 'step'}
+
+    def generate_inputs(self):
+        self.generate_text_input('min', _('Min value'), info=_("leave empty if you want to use the CDE's min value")),
+        self.generate_text_input('max', _('Max value'), info=_("leave empty if you want to use the CDE's max value"))
+        self.generate_text_input('left_label', _('Left label')),
+        self.generate_text_input('right_label', _('Right label')),
+        self.generate_text_input('step', _('Step')),
+
+
+class TimeWidgetSettings(JSONWidget):
+
+    def get_allowed_fields(self):
         return {'format'}
 
-    def generate_input(self, name, title, parsed, info=None):
-        value = parsed.get(name, '')
+    def generate_input(self, name, title, info=None):
+        value = self.parsed.get(name, '')
         selected_12hour = 'selected' if value == TimeWidget.AMPM else ''
         selected_24hour = 'selected' if value == TimeWidget.FULL else ''
         if not selected_12hour and not selected_24hour:
             selected_12hour = 'selected'
+        self.generate_select_input(
+            name, title, info=info,
+            options=[
+                {"value": TimeWidget.AMPM, "selected": selected_12hour, "text": "12-hour-format"},
+                {"value": TimeWidget.FULL, "selected": selected_24hour, "text": "24-hour-format"}
+            ]
+        )
 
-        input_str = f'''
-            <select name="{name}" id="{name}" onchange="saveJSON()">
-                <option value="{TimeWidget.AMPM}" {selected_12hour}> 12-hour format </option>
-                <option value="{TimeWidget.FULL}" {selected_24hour}> 24-hour format </option>
-            </select>'''
-        help_text = f'<div class="help">{info}</div>' if info else ''
-        return f"""
-            <div>
-                <label for="{name}">{title}</label>
-                {input_str}
-                {help_text}
-            </div>"""
-
-    def generate_inputs(self, parsed):
-        rows = [
-            self.generate_input('format', _('Format'), parsed, _("Format of time: 12-hour or 24-hour")),
-        ]
-        return "<br/>".join(rows)
-
-    def render(self, name, value, attrs=None, renderer=None):
-        parsed = {}
-        try:
-            parsed = json.loads(value)
-        except Exception:
-            pass
-
-        html = """
-             <div style="display:inline-grid" id="id_{name}">
-                {inputs}
-                <input type="hidden" name="{name}" value='{value}'/>
-             </div>""".format(inputs=self.generate_inputs(parsed), name=name, value=value)
-        javascript = """
-            function saveJSON() {
-                var value = $('#id_%s option:selected').val();
-                var obj = { format: value };
-                $("input[name='%s']").val(JSON.stringify(obj));
-            }
-            saveJSON();
-        """ % (name, name)
-        return mark_safe(f"""
-            {html}
-            <script>
-                {javascript}
-            </script>""")
+    def generate_inputs(self):
+        self.generate_input('format', _('Format'), _("Format of time: 12-hour or 24-hour")),
 
 
-class RadioSelectSettings(Widget):
-    @staticmethod
-    def get_allowed_fields():
+class RadioSelectSettings(JSONWidget):
+
+    def get_allowed_fields(self):
         return {'force_vertical'}
 
-    def generate_input(self, name, title, parsed, info=None):
-        value = parsed.get(name, False)
+    def generate_input(self, name, title, info=None):
+        value = self.parsed.get(name, False)
+        checked = "checked" if value else ""
+        self.generate_checkbox_input(name, title, info=info, checked=checked)
 
-        input_str = f'''
-            <input type="checkbox" name="{name}" id="{name}" onchange="saveJSON()" {"checked" if value else ""}>
-            '''
-        help_text = f'<div class="help">{info}</div>' if info else ''
-        return f"""
-            <div>
-                <label for="{name}">{title}</label>
-                {input_str}
-                {help_text}
-            </div>"""
-
-    def generate_inputs(self, parsed):
-        rows = [
-            self.generate_input(
-                'force_vertical', _('Force vertical layout'), parsed,
-                info=_('Always display each radio button on its own separate row')),
-        ]
-        return "<br/>".join(rows)
-
-    def render(self, name, value, attrs=None, renderer=None):
-        parsed = {}
-        try:
-            parsed = json.loads(value)
-        except Exception:
-            pass
-
-        html = """
-             <div style="display:inline-grid" id="id_{name}">
-                {inputs}
-                <input type="hidden" name="{name}" value='{value}'/>
-             </div>""".format(inputs=self.generate_inputs(parsed), name=name, value=value)
-        javascript = """
-            function saveJSON() {
-                var value = $('#id_%s input').prop("checked");
-                var obj = { force_vertical: value };
-                $("input[name='%s']").val(JSON.stringify(obj));
-            }
-            saveJSON();
-        """ % (name, name)
-        return mark_safe(f"""
-            {html}
-            <script>
-                {javascript}
-            </script>""")
+    def generate_inputs(self):
+        self.generate_input(
+            'force_vertical', _('Force vertical layout'),
+            info=_('Always display each radio button on its own separate row')
+        )
 
 
-class DurationWidgetSettings(Widget):
+class DurationWidgetSettings(JSONWidget):
 
-    @staticmethod
-    def get_allowed_fields():
+    def get_allowed_fields(self):
         return {'years', 'months', 'days', 'hours', 'minutes', 'seconds', 'weeks_only'}
 
-    def generate_input(self, name, title, parsed, info=None, default_value=False):
-        value = parsed.get(name, default_value)
+    def generate_input(self, name, title, info=None, default_value=False):
+        value = self.parsed.get(name, default_value)
         on_change = "update_weeks_only()" if name != "weeks_only" else "update_other_checkboxes()"
         checked = "checked" if value else ""
-        input_str = f'''
-            <input type="checkbox" name="{name}" id="{name}" {checked} onchange="{on_change}"/>
-        '''
-        help_text = f'<div class="help">{info}</div>' if info else ''
-        return f"""
-            <div>
-                <label for="{name}" style="width:175px">{title}</label>
-                {input_str}
-                {help_text}
-            </div>"""
+        self.generate_checkbox_input(name, title, info=info, checked=checked, onchange=on_change)
 
-    def generate_inputs(self, parsed):
-        is_empty = len(parsed) == 0
-        rows = [
-            self.generate_input('years', _("Display years input"), parsed, info=None, default_value=is_empty),
-            self.generate_input('months', _("Display months input"), parsed, info=None, default_value=is_empty),
-            self.generate_input('days', _("Display days input"), parsed, info=None, default_value=is_empty),
-            self.generate_input('hours', _("Display hours input"), parsed, info=None, default_value=is_empty),
-            self.generate_input('minutes', _("Display minutes input"), parsed, info=None, default_value=is_empty),
-            self.generate_input('seconds', _("Display seconds input"), parsed, info=None, default_value=is_empty),
-            '<span style="margin-bottom:5px;height:10px;border-bottom:1px solid #ccc"></span>',
-            self.generate_input('weeks_only', _("Display only weeks input"), parsed),
-        ]
-        return "<br/>".join(rows)
+    def generate_inputs(self):
+        is_empty = len(self.parsed) == 0
+        self.generate_input('years', _("Display years input"), info=None, default_value=is_empty),
+        self.generate_input('months', _("Display months input"), info=None, default_value=is_empty),
+        self.generate_input('days', _("Display days input"), info=None, default_value=is_empty),
+        self.generate_input('hours', _("Display hours input"), info=None, default_value=is_empty),
+        self.generate_input('minutes', _("Display minutes input"), info=None, default_value=is_empty),
+        self.generate_input('seconds', _("Display seconds input"), info=None, default_value=is_empty),
+        self.generate_input('weeks_only', _("Display only weeks input")),
 
     def render(self, name, value, attrs=None, renderer=None):
-        parsed = {}
-        try:
-            parsed = json.loads(value)
-        except Exception:
-            pass
+        self.parse_value(value)
+        self.generate_inputs()
 
-        html = """
-             <div style="display:inline-grid" id="id_{name}">
-                {inputs}
-                <input type="hidden" name="{name}" value='{value}'/>
-             </div>""".format(inputs=self.generate_inputs(parsed), name=name, value=value)
         javascript = """
-            function saveJSON() {
-                var inputs = $('#id_%s input[type=checkbox]');
-                var obj = {};
-                for (var i = 0; i < inputs.length; i++) {
-                    obj[inputs[i].name] = inputs[i].checked;
-                }
-                $("input[name='%s']").val(JSON.stringify(obj));
-            }
-
             function update_weeks_only() {
                 var value = $('#id_%s input[type=checkbox][name!="weeks_only"]').filter(function(idx, el) { return el.checked;});
                 if (value.length) {
@@ -245,7 +154,6 @@ class DurationWidgetSettings(Widget):
                 saveJSON();
             }
 
-
             function update_other_checkboxes() {
                 var value = $("#weeks_only").prop("checked");
                 if (value) {
@@ -253,11 +161,6 @@ class DurationWidgetSettings(Widget):
                 }
                 saveJSON();
             }
+        """ % (name, name)
 
-            saveJSON();
-        """ % (name, name, name, name)
-        return mark_safe(f"""
-            {html}
-            <script>
-                {javascript}
-            </script>""")
+        return self.render_widget(name, value, renderer, javascript)
