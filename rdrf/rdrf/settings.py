1686ffd81cd2b732a6e0d386111ecf6dc81f5ea8
Adam Smith <smitha50@qut.edu.au>
2020-03-05 15:54:07 +1000

    Add serving static content from s3 bucket

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -234,7 +234,9 @@ MANAGERS = ADMINS
 
 
 STATIC_ROOT = env.get('static_root', os.path.join(WEBAPP_ROOT, 'static'))
-STATIC_URL = '{0}/static/'.format(SCRIPT_NAME)
+EXTERNAL_BASE_URL = env.get(f'AWS_STATIC_CONTENT_URL', None)
+GIT_COMMIT_HASH = env.get('GIT_COMMIT_HASH', None)
+STATIC_URL = f"{EXTERNAL_BASE_URL}/{GIT_COMMIT_HASH}/" if EXTERNAL_BASE_URL and GIT_COMMIT_HASH else '{0}/static/'.format(SCRIPT_NAME)
 
 # TODO AH I can't see how this setting does anything
 # for local development, this is set to the static serving directory. For
