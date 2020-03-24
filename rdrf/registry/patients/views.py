b827d4d3b9af604066c1d37af4e63a9386c30ee5
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-03-24 23:11:17 +0200

    Restrict a patient to download only its own consent files

--- rdrf/registry/patients/views.py
+++ rdrf/registry/patients/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.http import Http404
+
+from rdrf.security.security_checks import security_check_user_patient
+
 from .models import PatientConsent
 
 
@@ -12,6 +16,7 @@ class ConsentFileView(View):
     @method_decorator(login_required)
     def get(self, request, consent_id=None, filename=""):
         consent = get_object_or_404(PatientConsent, pk=consent_id)
+        security_check_user_patient(request.user, consent.patient)
         if consent.form and consent.form.file:
             response = FileResponse(consent.form.file, content_type='application/octet-stream')
             response['Content-disposition'] = "filename=%s" % consent.filename
