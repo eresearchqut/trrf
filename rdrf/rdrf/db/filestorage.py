5d2e0e8769c142e746ca09973e21cb3920f4246a
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-03-29 16:46:04 +0300

    CDEFile security fixes

--- rdrf/rdrf/db/filestorage.py
+++ rdrf/rdrf/db/filestorage.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import logging
 import re
 from rdrf.models.definition.models import Registry, CDEFile
@@ -6,7 +7,10 @@ from rdrf.helpers.utils import models_from_mongo_key
 logger = logging.getLogger(__name__)
 
 __all__ = ["get_id", "delete_file_wrapper", "get_file",
-           "store_file", "store_file_by_key"]
+           "store_file", "store_file_by_key", "StorageFileInfo"]
+
+
+StorageFileInfo = namedtuple('StorageFileInfo', 'item filename uploaded_by patient')
 
 
 def get_id(value):
@@ -29,8 +33,10 @@ def delete_file_wrapper(file_ref):
     return None
 
 
-def store_file(registry_code, cde_code, file_obj, form_name=None, section_code=None):
+def store_file(registry_code, uploaded_by, patient, cde_code, file_obj, form_name=None, section_code=None):
     cde_file = CDEFile(registry_code=registry_code,
+                       uploaded_by=uploaded_by,
+                       patient=patient,
                        form_name=form_name,
                        section_code=section_code,
                        cde_code=cde_code,
@@ -44,10 +50,12 @@ def store_file(registry_code, cde_code, file_obj, form_name=None, section_code=N
     }
 
 
-def store_file_by_key(registry_code, patient_record, key, file_obj):
+def store_file_by_key(registry_code, patient_record, user, key, file_obj):
     registry = Registry.objects.get(code=registry_code)
     form, section, cde = models_from_mongo_key(registry, key)
     return store_file(registry_code,
+                      patient_record.user if patient_record else user,
+                      patient_record,
                       cde.code,
                       file_obj,
                       form.name,
@@ -60,6 +68,6 @@ oid_pat = re.compile(r"[0-9A-F]{24}", re.I)
 def get_file(file_id):
     try:
         cde_file = CDEFile.objects.get(id=file_id)
-        return cde_file.item, cde_file.filename
+        return StorageFileInfo(item=cde_file.item, filename=cde_file.filename, uploaded_by=cde_file.uploaded_by, patient=cde_file.patient)
     except CDEFile.DoesNotExist:
-        return None, None
+        return StorageFileInfo(item=None, filename=None, uploaded_by=None, patient=None)
