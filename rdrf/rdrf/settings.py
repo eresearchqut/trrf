d6dc57c4c5723886c8cdc9ba37423315b42228b2
Adam Smith <smitha50@qut.edu.au>
2020-04-20 08:56:11 +1000

    Retrieve HSTS seconds from env variable

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -24,7 +24,7 @@ SECURE_SSL_HOST = env.get("secure_ssl_host", False)
 SECURE_CONTENT_TYPE_NOSNIFF = env.get("secure_content_type_nosniff", PRODUCTION)
 SECURE_BROWSER_XSS_FILTER = env.get("secure_browser_xss_filter", PRODUCTION)
 SECURE_REDIRECT_EXEMPT = env.getlist("secure_redirect_exempt", [])
-SECURE_HSTS_SECONDS = 3600 # TODO: Bump to 1 week, 1 month, 1 year
+SECURE_HSTS_SECONDS = env.get("SECURE_HSTS_SECONDS", 3600)  # TODO: Bump to 1 week, 1 month, 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 X_FRAME_OPTIONS = env.get("x_frame_options", 'DENY')
