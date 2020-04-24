979e66a0584c00007a1434f3c4087963d042f9bb
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-04-24 07:54:51 +0300

    Added more blacklisted mime types

--- rdrf/rdrf/initial_data/blacklisted_mime_types.py
+++ rdrf/rdrf/initial_data/blacklisted_mime_types.py
@@ -11,149 +11,38 @@ def load_data(**kwargs):
 
 def init_blacklisted_file_types():
 
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/octet-stream',
-        defaults = {
-            'description': 'Executable files/Archives/Binary data'
-        }
-    )
+    mime_types_dict = {
+        'application/octet-stream': 'Executable files/Archives/Binary data',
+        'application/x-executable': 'Linux executable files',
+        'application/x-sharedlib': 'Linux shared libraries',
+        'text/x-shellscript': 'Linux/Unix shell scripts',
+        'application/x-sh': 'Bourne shell scripts',
+        'application/x-bsh': 'Bourne shell scripts',
+        'application/x-shar': 'Bourne shell scripts',
+        'text/x-script.sh': 'Bourne shell scripts',
+        'application/x-csh': 'C shell scripts',
+        'text/x-script.csh': 'C shell scripts',
+        'application/x-debian-package': 'Debian packages',
+        'application/x-msdownload': 'Microsoft applications',
+        'application/x-binary': 'Binary files',
+        'application/mac-binary': 'Mac binary files',
+        'application/macbinary': 'Mac binary files',
+        'application/x-macbinary': 'Mac binary files',
+        'application/x-javascript': 'JavaScript files',
+        'application/javascript': 'JavaScript files',
+        'application/ecmascript': 'JavaScript files',
+        'text/javascript': 'JavaScript files',
+        'text/ecmascript': 'JavaScript files',
+        'text/vbscript': 'VisualBasic script files',
+        'application/x-ms-application': 'MS application',
+        'application/x-java-applet': 'Java applet files',
+        'application/vnd.ms-pki.seccat': 'Windows security catalogs'
+    }
 
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-executable',
-        defaults = {
-            'description': 'Linux executable files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-sharedlib',
-        defaults = {
-            'description': 'Linux shared libraries'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='text/x-shellscript',
-        defaults = {
-            'description': 'Linux/Unix shell scripts'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-sh',
-        defaults = {
-            'description': 'Bourne shell scripts'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-bsh',
-        defaults = {
-            'description': 'Bourne shell scripts'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-shar',
-        defaults = {
-            'description': 'Bourne shell scripts'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='text/x-script.sh',
-        defaults = {
-            'description': 'Bourne shell scripts'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-csh',
-        defaults = {
-            'description': 'C shell scripts'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='text/x-script.csh',
-        defaults = {
-            'description': 'C shell scripts'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-debian-package',
-        defaults = {
-            'description': 'Debian packages'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-msdownload',
-        defaults = {
-            'description': 'Microsoft applications'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-binary',
-        defaults = {
-            'description': 'Binary files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/mac-binary',
-        defaults = {
-            'description': 'Mac binary files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/macbinary',
-        defaults = {
-            'description': 'Mac binary files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-macbinary',
-        defaults = {
-            'description': 'Mac binary files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/x-javascript',
-        defaults = {
-            'description': 'JavaScript files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/javascript',
-        defaults = {
-            'description': 'JavaScript files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='application/ecmascript',
-        defaults = {
-            'description': 'JavaScript files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='text/javascript',
-        defaults = {
-            'description': 'JavaScript files'
-        }
-    )
-
-    BlacklistedMimeType.objects.get_or_create(
-        mime_type='text/ecmascript',
-        defaults = {
-            'description': 'JavaScript files'
-        }
-    )
+    for mime_type, desc in mime_types_dict.items():
+        BlacklistedMimeType.objects.get_or_create(
+            mime_type=mime_type,
+            defaults = {
+                'description': desc
+            }
+        )
