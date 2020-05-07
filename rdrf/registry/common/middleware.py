0bf7bd006a549935e0617454a03949300f6f8d2b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-05-08 08:44:30 +1000

    Add logout to whitelisted urls

--- rdrf/registry/common/middleware.py
+++ rdrf/registry/common/middleware.py
@@ -60,6 +60,7 @@ class ForcePasswordResetMiddleware(MiddlewareMixin):
             'password_reset_done',
             'password_reset_confirm',
             'password_reset_complete',
+            'logout',
             'javascript-catalog',
             'js_reverse')
 
