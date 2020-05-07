10d8a67febd29a017e29e0a0aca87fa2150ba98b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-05-08 09:41:18 +1000

    Unify TFA and password reset middleware to fix bug when both are enforced

--- rdrf/registry/common/middleware.py
+++ rdrf/registry/common/middleware.py
@@ -7,34 +7,55 @@ from django.utils.deprecation import MiddlewareMixin
 logger = logging.getLogger(__name__)
 
 
-class EnforceTwoFactorAuthMiddleware(MiddlewareMixin):
+class UserSentryMiddleware(MiddlewareMixin):
     """
     This must be installed after
     :class:`~django.contrib.auth.middleware.AuthenticationMiddleware` and
     :class:`~django_otp.middleware.OTPMiddleware`.
     Users who are required to have two-factor authentication but aren't verified
     will always be redirected to the two-factor setup page.
+    Users who are required to reset their passwords will be redirected to the
+    password reset page
     """
 
+    whitelisted_views = [
+        'login',
+        'setup',
+        'qr',
+
+        'force_password_reset',
+        'password_reset_done',
+        'password_reset_confirm',
+        'password_reset_complete',
+
+        'logout',
+        'javascript-catalog',
+        'js_reverse']
+
     def process_request(self, request):
-        whitelisted_views = (
-            'two_factor:login',
-            'two_factor:setup',
-            'two_factor:qr',
-            'logout',
-            'javascript-catalog',
-            'js_reverse')
-        if any([reverse(v) in request.path_info for v in whitelisted_views]):
+        match = resolve(request.path)
+        if match.url_name in self.whitelisted_views:
             return None
 
         user = getattr(request, 'user', None)
         if user is None or user.is_anonymous:
             return None
 
+        for f in [self.verify_tfa, self.verify_password_reset]:
+            if redirect := f(user):
+                return redirect
+
+        return None
+
+    @staticmethod
+    def verify_tfa(user):
         if not user.is_verified() and user.require_2_fact_auth:
             return HttpResponseRedirect(reverse('two_factor:setup'))
 
-        return None
+    @staticmethod
+    def verify_password_reset(user):
+        if user.force_password_reset:
+            return HttpResponseRedirect(reverse('force_password_reset'))
 
 
 class NoCacheMiddleware:
@@ -51,28 +72,3 @@ class NoCacheMiddleware:
         if not response.has_header('Cache-Control'):
             add_never_cache_headers(response)
         return response
-
-
-class ForcePasswordResetMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        whitelisted_views = (
-            'force_password_reset',
-            'password_reset_done',
-            'password_reset_confirm',
-            'password_reset_complete',
-            'logout',
-            'javascript-catalog',
-            'js_reverse')
-
-        match = resolve(request.path)
-        if match.url_name in whitelisted_views:
-            return None
-
-        user = getattr(request, 'user', None)
-        if user is None or user.is_anonymous:
-            return None
-
-        if user.force_password_reset:
-            return HttpResponseRedirect(reverse('force_password_reset'))
-
-        return None
