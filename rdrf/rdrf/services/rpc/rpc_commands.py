8d55c07c1bbea4ee13852d67f964fa8f90176240
Tamas Szabo <szabtam@gmail.com>
2020-05-11 10:32:42 +1000

    Remove FH specific code

--- rdrf/rdrf/services/rpc/rpc_commands.py
+++ rdrf/rdrf/services/rpc/rpc_commands.py
@@ -5,49 +5,6 @@ from django.core.exceptions import PermissionDenied
 logger = logging.getLogger(__name__)
 
 
-def rpc_visibility(request, element):
-    user = request.user
-    if user.can("see", element):
-        return True
-
-
-def rpc_check_notifications(request):
-    from rdrf.models.definition.models import Notification
-    user = request.user
-    results = []
-    notifications = Notification.objects.filter(
-        to_username=user.username, seen=False).order_by('-created')
-    for notification in notifications:
-        results.append({"message": notification.message,
-                        "from_user": notification.from_username, "link": notification.link})
-    return results
-
-
-def rpc_dismiss_notification(request, notification_id):
-    from rdrf.models.definition.models import Notification
-    status = False
-    try:
-        notification = Notification.objects.get(pk=int(notification_id))
-        notification.seen = True
-        notification.save()
-        status = True
-    except Exception as ex:
-        logger.error("could not mark notification with id %s as seen: %s" %
-                     (notification_id, ex))
-    return status
-
-
-def rpc_fh_patient_is_index(request, patient_id):
-    from registry.patients.models import Patient
-    patient = Patient.objects.get(pk=patient_id)
-    if patient.in_registry("fh"):
-        is_index = patient.get_form_value(
-            "fh", "ClinicalData", "fhDateSection", "CDEIndexOrRelative") != "fh_is_relative"
-        return is_index
-    else:
-        return False
-
-
 def rpc_reporting_command(request, query_id, registry_id, command, arg):
     # 2 possible commands/invocations client side from report definition screen:
     # get_field_data: used to build all the checkboxes for client
