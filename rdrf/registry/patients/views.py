81d75a4c021e513232fc073f8a244c3b9953b142
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-05-17 17:37:14 +0300

    Code cleanup

--- rdrf/registry/patients/views.py
+++ rdrf/registry/patients/views.py
@@ -1,17 +1,16 @@
 
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import FileResponse, Http404, JsonResponse
 from django.shortcuts import get_object_or_404
-from django.template.loader import render_to_string
-from django.views.generic import View
+from django.views.generic.base import View
 
 from rdrf.db.filestorage import virus_checker_result
+from rdrf.helpers.view_helper import FileErrorHandlingMixin
 from rdrf.security.security_checks import security_check_user_patient
 
 from .models import PatientConsent, upload_patient_consent_to
 
 
-class ConsentFileView(View):
+class ConsentFileView(FileErrorHandlingMixin, View):
 
     def get(self, request, consent_id=None, filename=""):
         consent = get_object_or_404(PatientConsent, pk=consent_id)
@@ -19,9 +18,8 @@ class ConsentFileView(View):
         check_status = request.GET.get('check_status', '')
         need_status_check = check_status and check_status.lower() == 'true'
         if need_status_check:
-            file_name = upload_patient_consent_to(consent, consent.filename)
             return JsonResponse({
-                "response": virus_checker_result(file_name),
+                "response": virus_checker_result(upload_patient_consent_to(consent, consent.filename)),
             })
 
         if consent.form and consent.form.file:
