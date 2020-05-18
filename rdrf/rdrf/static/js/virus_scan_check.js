94fcc14c6f8d1416081a26ef0df198f5b97081e0
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-05-18 09:15:08 +0300

    Cleanup js code

---
+++ rdrf/rdrf/static/js/virus_scan_check.js
@@ -0,0 +1,34 @@
+function update_status_div(base_id, status, is_visible) {
+    var target = $("#" + base_id + "_" + status);
+    is_visible ? target.show(): target.hide();
+}
+
+function check_virus_scan_status(base_url, base_id, value, interval) {
+    $.ajax({
+        url: base_url + "?check_status=true",
+        type: "GET",
+        success: function(data) {
+            var status = data['response'];
+            if (status == 'scanning') {
+                update_status_div(base_id, 'scanning', true);
+            } else {
+                update_status_div(base_id, 'scanning', false);
+                clearInterval(interval);
+                if (status == 'infected') {
+                    update_status_div(base_id, 'infected', true);
+                } else if (status == 'not found') {
+                    update_status_div(base_id, 'notfound', true);
+                } else if (status == 'clean') {
+                    $("#" + base_id + "_link").html('<a href="' + base_url +'">' + value + '</a>');
+                }
+            }
+        }
+    });
+}
+
+function setup_virus_status_check(base_url, base_id, value, interval_ms) {
+    check_virus_scan_status(base_url, base_id, value);
+    var _interval = setInterval( function() {
+        check_virus_scan_status(base_url, base_id, value, _interval);
+    }, interval_ms);
+}
