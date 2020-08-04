5dd903799225f63d00bc1afe39c93d0a29e11025
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-08-04 11:16:44 +0300

    Moved session refresh retries count to settings

--- rdrf/rdrf/views/session_refresh_view.py
+++ rdrf/rdrf/views/session_refresh_view.py
@@ -1,15 +1,14 @@
+from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
-
-_MAX_REFRESH_RETRIES = 3
 
 
 @require_GET
 def session_refresh(request):
     num_retries = request.session.get('num_retries')
     if num_retries is None:
-        request.session['num_retries'] = _MAX_REFRESH_RETRIES
-        num_retries = _MAX_REFRESH_RETRIES
+        request.session['num_retries'] = settings.MAX_SESSION_REFRESH_RETRIES
+        num_retries = settings.MAX_SESSION_REFRESH_RETRIES
     elif num_retries > 0:
         num_retries -= 1
         request.session['num_retries'] = num_retries
