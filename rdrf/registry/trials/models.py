553650b13905eec06dd78115e0879b26268c5114
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-12-01 10:44:23 +1000

    Add sequence indices to trial models

--- rdrf/registry/trials/models.py
+++ rdrf/registry/trials/models.py
@@ -4,25 +4,6 @@ from rdrf.models.definition.models import Registry
 from registry.patients.models import Patient
 
 
-class Intervention(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-
-    class Meta:
-        abstract = True
-
-
-class NofOneTreatment(Intervention):
-    blinded_title = models.CharField(max_length=20)
-
-    class Meta:
-        verbose_name = "N-of-1 Treatment"
-        verbose_name_plural = "N-of-1 Treatments"
-
-    def __str__(self):
-        return f"{self.blinded_title} ({self.title})"
-
-
 class NofOneTrial(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(blank=True)
@@ -36,9 +17,25 @@ class NofOneTrial(models.Model):
         return f"{self.title} ({self.registry.code})"
 
 
+class NofOneTreatment(models.Model):
+    title = models.CharField(max_length=100)
+    blinded_title = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    trial = models.ForeignKey(NofOneTrial, on_delete=models.CASCADE, related_name="trials")
+
+    class Meta:
+        verbose_name = "N-of-1 Treatment"
+        verbose_name_plural = "N-of-1 Treatments"
+
+    def __str__(self):
+        return f"{self.blinded_title} ({self.title})"
+
+
 class NofOneArm(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.PROTECT, blank=True, null=True)
     trial = models.ForeignKey(NofOneTrial, on_delete=models.PROTECT, related_name="arms")
+
+    sequence_index = models.IntegerField()
 
     class Meta:
         verbose_name = "N-of-1 Arm"
@@ -55,6 +52,8 @@ class NofOneArm(models.Model):
 class NofOneCycle(models.Model):
     arm = models.ForeignKey(NofOneArm, on_delete=models.PROTECT, related_name="cycles")
 
+    sequence_index = models.IntegerField()
+
     class Meta:
         verbose_name = "N-of-1 Cycle"
         verbose_name_plural = "N-of-1 Cycles"
@@ -65,6 +64,14 @@ class NofOneCycle(models.Model):
         return f"{cycle_treatments} - ({periods.first().start} -> {periods.last().end})"
 
     @property
+    def start(self):
+        return self.ordered_periods.first().start
+
+    @property
+    def end(self):
+        return self.ordered_periods.last().end
+
+    @property
     def ordered_periods(self):
         return self.periods.all().order_by("start")
 
@@ -73,8 +80,11 @@ class NofOnePeriod(models.Model):
     cycle = models.ForeignKey(NofOneCycle, on_delete=models.PROTECT, related_name="periods")
     treatment = models.ForeignKey(NofOneTreatment, on_delete=models.PROTECT, related_name="periods")
 
+    sequence_index = models.IntegerField()
+
     start = models.DateTimeField(blank=True, null=True)
     duration = models.DurationField(blank=True, null=True)
+    washout = models.DurationField(blank=True, null=True)
 
     class Meta:
         verbose_name = "N-of-1 Period"
