1110fb3fbdb347478208c8fa45ee9f53f6d2bce0
ScriptSmith <ScriptSmith@users.noreply.github.com>
2021-03-03 12:36:26 +1000

    Re-activate de-activated users on password reset

--- rdrf/rdrf/urls.py
+++ rdrf/rdrf/urls.py
@@ -137,8 +137,7 @@ normalpatterns += [
             kwargs={'password_reset_form': RDRFPasswordResetForm}, name='password_reset'),
     re_path(r'^password_reset/done/?$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/?$',
-            auth_views.PasswordResetConfirmView.as_view(),
-            kwargs={'set_password_form': RDRFSetPasswordForm},
+            auth_views.PasswordResetConfirmView.as_view(form_class=RDRFSetPasswordForm),
             name='password_reset_confirm'),
     re_path(r'^reset/done/?$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
