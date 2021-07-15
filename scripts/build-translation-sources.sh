9889880452ca2cd3004eb969d85e6a70fb6d88a7
ScriptSmith <ScriptSmith@users.noreply.github.com>
2021-07-15 11:19:00 +1000

    Add translation source building script

---
+++ scripts/build-translation-sources.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Creates the translation source from the app source code, and the supplied registry definition
+# Used as input for crowdin translation platform
+#
+# And contain registry definition file named $APPLICATION_NAME.yaml in the project root
+
+if [ -z "$1" ]
+then
+    echo "Registry definition argument"
+    exit 1
+fi
+
+# Build django strings
+docker-compose run --rm -w "/app" runserver django-admin makemessages -l en
+
+# Build registry definition strings
+docker-compose run --rm -w "/app" -e REGISTRY_DEFINITION="$1" runserver bash -c 'django-admin create_translation_file --yaml_file "$REGISTRY_DEFINITION" --system_po_file "translations/locale/en/LC_MESSAGES/django.po" >> "translations/locale/en/LC_MESSAGES/django.po"'
