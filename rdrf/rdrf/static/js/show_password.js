e689e1e9ddca3e4be3f736e5e6698f101b873b52
Paola Pettit <paola.pettit@qut.edu.au>
2021-10-01 15:15:08 +1000

    PR Feedback: * Add an option for users to turn off the automatic hiding of passwords after predefined time has elapsed * Passwords on Password reset form are showing the literal value "None" when there's no existing values to display

--- rdrf/rdrf/static/js/show_password.js
+++ rdrf/rdrf/static/js/show_password.js
@@ -1,23 +1,50 @@
-function initToggleShowPassword($passwordFields, $passwordToggle) {
+function initToggleShowPassword($password_fields, $password_toggle) {
 
-    let timeoutId;
+    /* Private Fields */
+    let timeout_id;
+    let enable_timeout = true;
 
-    const timeout_ms = 60000;
+    const ms_in_sec = 1000;
+    const timeout_ms = 60 * ms_in_sec;
+    const $timeout_message = $("<div />", {
+            class: "small text-muted mb-3",
+            style: "display: none",
+            html: "Passwords will be automatically hidden after " + (timeout_ms / ms_in_sec) + " seconds. <br />" +
+                "<button type='button' class='p-0 btn btn-link btn-sm turn-off-timeout'>Don't automatically hide passwords.<a/>"
+    });
+
+    /* Private Functions */
     const toggleFieldType = function(type) {
-        $passwordFields.attr("type", type);
+        $password_fields.attr("type", type);
     }
     const resetPasswordFields = function() {
         toggleFieldType("password");
-        $passwordToggle.prop("checked", false);
+        $timeout_message.hide();
+        $password_toggle.prop("checked", false);
     };
 
-    $passwordToggle.on('click', function() {
-        if ($passwordToggle.is(":checked")) {
-            toggleFieldType("text")
-            timeoutId = setTimeout(resetPasswordFields, timeout_ms);
+    /* Init */
+    $password_toggle.on('click', function() {
+        if ($password_toggle.is(":checked")) {
+            toggleFieldType("text");
+
+            if (enable_timeout) {
+                $timeout_message.show();
+                timeout_id = setTimeout(resetPasswordFields, timeout_ms);
+            }
         } else {
             toggleFieldType("password")
-            clearTimeout(timeoutId);
+            $timeout_message.hide();
+            clearTimeout(timeout_id);
         }
     });
+
+    $timeout_message.find(".turn-off-timeout").on("click", function() {
+        enable_timeout = false;
+        clearTimeout(timeout_id);
+        $timeout_message.hide();
+    });
+
+    $timeout_message.insertAfter($password_toggle.parent("div"));
+
 }
