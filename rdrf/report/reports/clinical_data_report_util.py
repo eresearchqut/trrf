46e3157c20c066ef670f98da20e79eefd8c79329
Paola Pettit <paola.pettit@qut.edu.au>
2022-03-28 12:09:51 +1000

    Filter clinical data summary by patients relevant to a given report (as per user defined filters)

--- rdrf/report/reports/clinical_data_report_util.py
+++ rdrf/report/reports/clinical_data_report_util.py
@@ -9,61 +9,12 @@ from rdrf.helpers.utils import mongo_key, get_form_section_code
 from rdrf.models.definition.models import RDRFContext, ContextFormGroup, RegistryForm, Section, \
     CommonDataElement
 from report.models import ReportCdeHeadingFormat
+from report.schema.schema import list_patients_query
 
 logger = logging.getLogger(__name__)
 
 
 class ClinicalDataReportUtil:
-
-    def __context_lookup(self, context_id):
-        return RDRFContext.objects.get(pk=context_id)
-
-    def __extract_cde_data(self, form_name, section_code, processed_cdes, allow_multiple, cdes):
-        if allow_multiple:
-            for cde_entry in cdes:
-                return self.__extract_cde_data(form_name, section_code, processed_cdes, False, cde_entry)
-        cdes_data = {}
-        for cde in cdes:
-            cde_code = cde['code']
-            if mongo_key(form_name, section_code, cde_code) in self.report_cdes:
-                cde_value = cde['value']
-                processed_cde = processed_cdes.get(cde_code, {})
-                cnt_cde_values = len(cde_value) if type(cde_value) is list else 1
-                cdes_data.setdefault(cde_code, {
-                    'count': max(processed_cde.get('count', 1), cnt_cde_values)
-                })
-        return cdes_data
-
-    def __extract_section_data(self, form_name, processed_sections, sections):
-        sections_data = {}
-        for section in sections:
-            section_code = section['code']
-            section_cdes = section['cdes']
-            processed_cdes = processed_sections.get(section_code, {}).get('cdes', {})
-
-            cdes_data = self.__extract_cde_data(form_name,
-                                                section_code,
-                                                processed_cdes,
-                                                section.get('allow_multiple', False),
-                                                section_cdes)
-
-            if cdes_data:
-                sections_data.setdefault(section_code, {
-                    'count': max(len(section_cdes), processed_cdes.get('count', 1)),
-                    'cdes': cdes_data
-                })
-        return sections_data
-
-    def __extract_forms_data(self, processed_forms, forms):
-        forms_data = {}
-        for form in forms:
-            form_name = form['name']
-            processed_sections = processed_forms.get(form_name, {})
-
-            sections_data = self.__extract_section_data(form_name, processed_sections, form['sections'])
-            if sections_data:
-                forms_data.setdefault(form_name, sections_data)
-        return forms_data
 
     def __cde_heading(self, cde, heading_format):
         if heading_format == ReportCdeHeadingFormat.ABBR_NAME.value:
@@ -120,7 +71,7 @@ class ClinicalDataReportUtil:
         label = f'{form_label_prefix}_{section_label}_{cde_heading}'
         return key, label
 
-    def __clinical_data_summary(self, cde_keys):
+    def __clinical_data_summary(self, patient_ids, cde_keys):
         sql = """
             WITH cd_sections AS (
                      SELECT id, form_name, jsonb_extract_path_text(cd_forms.sections, 'code') section_code, jsonb_array_elements(cd_forms.sections#>'{cdes}') cdes
@@ -130,6 +81,7 @@ class ClinicalDataReportUtil:
                               FROM rdrf_clinicaldata
                               WHERE django_model = 'Patient'
                               AND   collection = 'cdes'
+                              AND   django_id = ANY(%(patient_ids)s)
                               ) AS cd
                      ) as cd_forms
                ), sections_summary AS (
@@ -178,7 +130,7 @@ class ClinicalDataReportUtil:
 
         summary = {}
         with connections['clinical'].cursor() as cursor:
-            cursor.execute(sql, {'cde_delim': settings.FORM_SECTION_DELIMITER, 'cde_keys': cde_keys})
+            cursor.execute(sql, {'patient_ids': patient_ids, 'cde_delim': settings.FORM_SECTION_DELIMITER, 'cde_keys': cde_keys})
             for row in cursor.fetchall():
                 form_name, section_code, max_cnt_sections, cde_code, max_cde_value_cnt = row
                 section_cdes = summary.setdefault(form_name, {}).setdefault(section_code, {'count': max_cnt_sections, 'cdes': {}}).get('cdes')
@@ -187,7 +139,7 @@ class ClinicalDataReportUtil:
         return summary
 
     def form_section_cde_sort_order(self, cde_keys):
-        sort_order_lookup = {}  # {formkey: {order: 1, sections: {sectionkey: {order: 1, cdes: {cdekey: order: 1}}}}
+        sort_order_lookup = {}  # {formkey: {order: 1, sections: {sectionkey: {order: 1, cdes: {cdekey: 1}}}}
 
         for cde_i, cde_key in enumerate(cde_keys):
             form_name, section_code, cde_code = get_form_section_code(cde_key)
@@ -200,58 +152,64 @@ class ClinicalDataReportUtil:
 
         return sort_order_lookup
 
-    def csv_headers(self, user, report_design):
-        # TODO filter patient records by patient filters. If we don't we may get more csv headers than we need.
-        # patients = list_patients_query(user,
-        #                                report_design.registry.code,
-        #                                [cq.code for cq in report_design.filter_consents.all()],
-        #                                [wg.id for wg in report_design.filter_working_groups.all()]).values_list("id", flat=True)
 
+    def sorted_forms(self, unsorted_forms_dict, sort_order_lookup):
+        return dict(sorted(unsorted_forms_dict.items(), key=lambda item: sort_order_lookup[item[0]]['order'])).items()
+
+
+    def sorted_sections(self, unsorted_sections_dict, sort_order_lookup, form_name):
+        return dict(sorted(unsorted_sections_dict.items(), key=lambda item: sort_order_lookup[form_name]['sections'][item[0]]['order'])).items()
+
+
+    def sorted_cdes(self, unsorted_cdes_dict, sort_order_lookup, form_name, section_code):
+        return dict(sorted(unsorted_cdes_dict.items(), key=lambda item: sort_order_lookup[form_name]['sections'][section_code]['cdes'][item[0]])).items()
+
+
+    def get_max_count_cfg_entries(self, cfg):
+        # Get counts of entries per patient (object_id) for this context form group
+        # Take the first count in the result set, which works out to be the max count of number of patient entries
+        max_cnt_entries = \
+            RDRFContext.objects.filter(context_form_group=cfg) \
+                .values('context_form_group', 'object_id') \
+                .annotate(cnt_entries=Count('object_id')).order_by('-cnt_entries')[:1]
+
+        return max_cnt_entries.first()['cnt_entries'] if max_cnt_entries else 1
+
+
+    def get_cfg_data(self, cfg, cfgs_lookup, heading_format):
+        cfg_lookup = cfgs_lookup.get(cfg.code, None)
+        if not cfg_lookup:
+            cfg_lookup = {'count': self.get_max_count_cfg_entries(cfg),
+                          'header': self.__cfg_heading(cfg, heading_format)}
+            cfgs_lookup[cfg.code] = cfg_lookup
+
+        return cfg_lookup["header"], cfg_lookup['count']
+
+    def csv_headers(self, user, report_design):
         cde_keys = list(report_design.reportclinicaldatafield_set.order_by('id').values_list('cde_key', flat=True))
 
         # Step 1 - Summarise all clinical data
-        cd_summary = self.__clinical_data_summary(cde_keys)
+        patient_ids = list(list_patients_query(user,
+                                               report_design.registry.code,
+                                               [cq.code for cq in report_design.filter_consents.all()],
+                                               [wg.id for wg in report_design.filter_working_groups.all()]).values_list("id", flat=True))
+        cd_summary = self.__clinical_data_summary(patient_ids, cde_keys)
 
-        cfgs_lookup = {}  # e.g. {'cfgcode': {'count': 1, 'heading': 'formatted heading'}}
-
-        # TODO move me
-        def get_max_cnt_entries(cfg):
-            # TODO can this be nicer?
-            max_cnt_entries = \
-                RDRFContext.objects.filter(context_form_group=cfg)\
-                                   .values('context_form_group', 'object_id')\
-                                   .annotate(cnt_entries=Count('object_id')).order_by('-cnt_entries')[:1]
-
-            if max_cnt_entries:
-                return max_cnt_entries.first()['cnt_entries']
-            else:
-                return 1
 
         # Step 2 - Generate headers from summary
-        headers = OrderedDict()
-
-        # Determine ordering
+        cfgs_lookup = {}  # e.g. {'cfgcode': {'count': 1, 'heading': 'formatted heading'}}
         sort_order_lookup = self.form_section_cde_sort_order(cde_keys)
 
-        # Sort forms by expected order
-        sorted_cd_summary = dict(sorted(cd_summary.items(), key=lambda item: sort_order_lookup[item[0]]['order']))
+        headers = OrderedDict()
 
-        for form_name, form_data in sorted_cd_summary.items():
+        for form_name, form_data in self.sorted_forms(cd_summary, sort_order_lookup):
             form = RegistryForm.objects.get(name=form_name)
             form_heading = self.__form_heading(form, report_design.cde_heading_format)
 
             # TODO fix this so we only get the cfgs relevant to the contexts of the clinical data (separate sql TODO)
             for cfg in ContextFormGroup.objects.filter(items__registry_form=form):
 
-                # TODO move to a function
-                cfg_lookup = cfgs_lookup.get(cfg.code, None)
-                if not cfg_lookup:
-                    cfg_lookup = {'count': get_max_cnt_entries(cfg),
-                                  'header': self.__cfg_heading(cfg, report_design.cde_heading_format)}
-                    cfgs_lookup[cfg.code] = cfg_lookup
-
-                cfg_heading = cfg_lookup["header"]
-                longitudinal_entries_cnt = cfg_lookup['count']
+                cfg_heading, longitudinal_entries_cnt = self.get_cfg_data(cfg, cfgs_lookup, report_design.cde_heading_format)
 
                 for cfg_i in range(longitudinal_entries_cnt):
                     form_key_prefix, form_label_prefix = self.__form_header_parts(cfg, cfg_heading, cfg_i, form, form_heading)
@@ -261,16 +219,12 @@ class ClinicalDataReportUtil:
                     else:
                         form_key_suffix = ''
 
-                    sorted_sections = dict(sorted(form_data.items(), key=lambda item: sort_order_lookup[form_name]['sections'][item[0]]['order']))
-
-                    for section_code, section_data in sorted_sections.items():
+                    for section_code, section_data in self.sorted_sections(form_data, sort_order_lookup, form_name):
                         section = Section.objects.get(code=section_code)
                         section_heading = self.__section_heading(section, report_design.cde_heading_format)
                         for section_i in range(section_data['count']):
 
-                            sorted_cdes = dict(sorted(section_data['cdes'].items(), key=lambda item: sort_order_lookup[form_name]['sections'][section_code]['cdes'][item[0]]))
-
-                            for cde_code, cde_data in sorted_cdes.items():
+                            for cde_code, cde_data in self.sorted_cdes(section_data['cdes'], sort_order_lookup, form_name, section_code):
 
                                 cde = CommonDataElement.objects.get(code=cde_code)
                                 cde_heading = self.__cde_heading(cde, report_design.cde_heading_format)
