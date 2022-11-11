d27dfd1de6a2d3a99913f6898928c3a87fa52c25
ScriptSmith <ScriptSmith@users.noreply.github.com>
2022-11-11 16:58:21 +1000

    eval conditions

--- rdrf/rdrf/services/io/notifications/longitudinal_followups.py
+++ rdrf/rdrf/services/io/notifications/longitudinal_followups.py
@@ -12,20 +12,36 @@ from registry.patients.models import LongitudinalFollowupEntry
 logger = logging.getLogger(__name__)
 
 
+def evaluate_condition_patient(patient):
+    def evaluate_condition(longitudinal_followup_entry):
+        if longitudinal_followup_entry.longitudinal_followup.condition:
+            return eval(longitudinal_followup_entry.longitudinal_followup.condition, {'patient': patient})
+        else:
+            return True
+
+    return evaluate_condition
+
+
 def send_longitudinal_followups():
     entries = LongitudinalFollowupEntry.objects.filter(
         state=LongitudinalFollowupEntry.QueueState.PENDING,
         send_at__lte=datetime.datetime.now(),
     ).order_by("patient").iterator(chunk_size=100)
-
     for patient, entries in itertools.groupby(entries, lambda e: e.patient):
-        entries = list(entries)
+        entries = list(filter(evaluate_condition_patient(patient), entries))
+
+        if not entries:
+            continue
+
         now = datetime.datetime.now()
         with transaction.atomic():
-            sent_successfully, has_disabled = process_notification(patient.rdrf_registry.first().code, EventType.LONGITUDINAL_FOLLOWUP, {
-                "patient": patient,
-                "entries": serializers.serialize("json", entries),
-            })
+            sent_successfully, has_disabled = process_notification(
+                patient.rdrf_registry.first().code,
+                EventType.LONGITUDINAL_FOLLOWUP, {
+                    "patient": patient,
+                    "entries": serializers.serialize("json", entries),
+                }
+            )
 
             if sent_successfully:
                 for entry in entries:
