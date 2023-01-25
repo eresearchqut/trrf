d606589914ed1c0702d31e5abdbd2ccf13191f2c
Paola Pettit <paola.pettit@qut.edu.au>
2023-01-25 15:49:37 +1000

    Add xnat client and wiremock mappings

---
+++ clients/xnat/generated/openapi_client/apis/paths/data_projects_project_id_subjects_subject_id_experiments.py
@@ -0,0 +1,7 @@
+from openapi_client.paths.data_projects_project_id_subjects_subject_id_experiments.get import ApiForget
+
+
+class DataProjectsProjectIdSubjectsSubjectIdExperiments(
+    ApiForget,
+):
+    pass
