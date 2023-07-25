7b952f5ccbf25b295c55dcf6ac4ed6ae9fa21bc5
LexieLiu <liul46@QUT-PA00157338.qut.edu.au>
2023-07-25 14:14:54 +1000

    [ERE-2117] Update has_subscribable_email function

--- rdrf/rdrf/templatetags/has_subscribable_email.py
+++ rdrf/rdrf/templatetags/has_subscribable_email.py
@@ -4,17 +4,10 @@ from rdrf.models.definition.models import EmailNotification
 register = template.Library()
 
 
-# check whether the registry the user belongs to has subscribable email notifications.
 @register.filter()
 def has_subscribable_email(user):
-    if user.num_registries == 0:
-        return False
     registries = user.registry.all()
-    for registry in registries:
-        email_notification_data = EmailNotification.objects.filter(registry=registry).values()
-        for data in email_notification_data:
-            if data['subscribable']:
-                return True
-            pass
+    if EmailNotification.objects.filter(registry__in=registries, subscribable=True).exists():
+        return True
 
     return False
