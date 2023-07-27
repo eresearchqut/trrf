82e0199353602d06ec9e2b2a617b93075f19911b
Paola Pettit <paola.pettit@qut.edu.au>
2023-07-28 07:52:02 +1000

    PR Fixes

--- rdrf/rdrf/helpers/utils.py
+++ rdrf/rdrf/helpers/utils.py
@@ -862,7 +862,7 @@ def validate_abbreviated_name(value):
 
 
 def validate_file_extension_format(value):
-    if re.match(r'^\.([^\.]*)', value) is None:
+    if re.match(r'^\.[^.]+$', value) is None:
         raise ValidationError(_('File extension '
                                 'is invalid. '
                                 'File extension should begin with a period followed by some characters e.g: .pdf'))
