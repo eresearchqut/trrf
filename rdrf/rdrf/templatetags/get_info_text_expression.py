058cbbc338f8346f04a73579b6d7e9673e0b30f4
LexieLiu <lexie.liu@qut.edu.au>
2023-08-29 15:09:00 +1000

    [ERE-2181] Default consent video language to user's language

--- rdrf/rdrf/templatetags/get_info_text_expression.py
+++ rdrf/rdrf/templatetags/get_info_text_expression.py
@@ -1,5 +1,7 @@
 from django import template
 from django.utils.translation import gettext as _
+from django.template import Template, Context
+from django.conf import settings
 
 from rdrf.models.definition.models import ConsentSection
 import logging
@@ -10,7 +12,7 @@ register = template.Library()
 
 
 @register.filter()
-def get_info_text_expression(fields):
+def get_info_text_expression(fields, current_lang='en'):
     #  ['customconsent_15_13_21', 'customconsent_15_13_22']
     if len(fields) > 0:
         consent_field = fields[0]
@@ -21,7 +23,13 @@ def get_info_text_expression(fields):
         try:
             consent_section_model = ConsentSection.objects.get(pk=consent_section_model_pk)
             if consent_section_model.information_text:
-                return _(consent_section_model.information_text)
+                information_text = Template(consent_section_model.information_text)
+                context = Context({
+                    'all_available_langs': settings.LANGUAGES,
+                    'current_language': current_lang,
+                })
+                information_text = information_text.render(context)
+                return _(information_text)
 
         except BaseException:
             return
