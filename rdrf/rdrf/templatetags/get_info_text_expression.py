155bf3e61ffd04f756b524c82c65ca3b6764a6e7
Lexiel46 <139732653+Lexiel46@users.noreply.github.com>
2023-10-30 13:46:06 +1000

    [ERP-694] Consent page doesn't support pseudo language (#652)
    Co-authored-by: lexie <lexie.liu@qut.edu.au>

--- rdrf/rdrf/templatetags/get_info_text_expression.py
+++ rdrf/rdrf/templatetags/get_info_text_expression.py
@@ -25,9 +25,11 @@ def get_info_text_expression(fields, current_language='en'):
             consent_section_model = ConsentSection.objects.get(pk=consent_section_model_pk)
             if consent_section_model.information_text:
                 information_text = Template(consent_section_model.information_text)
+                languages = settings.LANGUAGES
+                formatted_languages = {k.lower(): v for k, v in dict(languages).items()}
                 context = Context({
-                    'all_available_languages': json.dumps(settings.LANGUAGES),
-                    'current_language': json.dumps([current_language['code'], current_language['name_local']]),
+                    'all_available_languages': json.dumps(languages),
+                    'current_language': json.dumps([current_language, formatted_languages[current_language]]),
                 })
                 information_text = information_text.render(context)
                 return _(information_text)
