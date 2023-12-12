6608150311efa1040daa034be27df0392fc40d7e
lexie <lexie.liu@qut.edu.au>
2023-12-13 07:55:34 +1000

    [ERP-881] Add NO_TRANSLATION to RegistryFeatures

--- rdrf/rdrf/templatetags/get_translate_value.py
+++ rdrf/rdrf/templatetags/get_translate_value.py
@@ -1,12 +1,14 @@
 from django import template
 from rdrf.models.definition.models import Registry
+from rdrf.helpers.registry_features import RegistryFeatures
 
 register = template.Library()
 
 
 @register.filter
 def get_translate_value(registry):
+    no_translation = RegistryFeatures.NO_TRANSLATION
     if registry:
-        return 'no' if registry.has_feature('no_translation') else 'yes'
+        return 'no' if registry.has_feature(no_translation) else 'yes'
 
-    return 'no' if all(registry.has_feature('no_translation') for registry in Registry.objects.all()) else 'yes'
+    return 'no' if all(registry.has_feature(no_translation) for registry in Registry.objects.all()) else 'yes'
