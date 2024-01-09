a842fd51a80175f33ebf8781446e4fab9276d6e4
lexie <lexie.liu@qut.edu.au>
2024-01-10 09:11:51 +1000

    [ERP-881]Remove no_translation

--- rdrf/rdrf/templatetags/get_translate_value.py
+++ rdrf/rdrf/templatetags/get_translate_value.py
@@ -7,8 +7,7 @@ register = template.Library()
 
 @register.filter
 def get_translate_value(registry):
-    no_translation = RegistryFeatures.NO_TRANSLATION
     if registry:
-        return 'no' if registry.has_feature(no_translation) else 'yes'
+        return 'no' if registry.has_feature(RegistryFeatures.NO_TRANSLATION) else 'yes'
 
-    return 'no' if all(registry.has_feature(no_translation) for registry in Registry.objects.all()) else 'yes'
+    return 'no' if all(registry.has_feature(RegistryFeatures.NO_TRANSLATION) for registry in Registry.objects.all()) else 'yes'
