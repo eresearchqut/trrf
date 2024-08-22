832c130f5a8b1ea2d19ba932282b17a23c9d1bb0
ScriptSmith <ScriptSmith@users.noreply.github.com>
2024-08-22 15:23:51 +1000

    Add formatter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,3 +2,4 @@
 
 echo "Linting your python code"
 docker run --rm --volume $(pwd):/io ghcr.io/astral-sh/ruff check . || exit 1
+docker run --rm --volume $(pwd):/io ghcr.io/astral-sh/ruff format --check . || exit 1
