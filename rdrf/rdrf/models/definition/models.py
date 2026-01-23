5ea4fb09c2d1a2b9a564a4284da94488c5104367
ScriptSmith <ScriptSmith@users.noreply.github.com>
2026-01-23 14:22:23 +1000

    Lint fix

--- rdrf/rdrf/models/definition/models.py
+++ rdrf/rdrf/models/definition/models.py
@@ -1078,8 +1078,9 @@ class RegistryForm(models.Model):
 @receiver([post_save, post_delete], sender=Section)
 @receiver([post_save, post_delete], sender=CommonDataElement)
 def registry_form_definition_changed(sender, instance, **kwargs):
-    from rdrf.forms.dsl.parse_utils import clear_prefetched_form_data_cache
     from report.schema import clear_dynamic_schema_cache
+
+    from rdrf.forms.dsl.parse_utils import clear_prefetched_form_data_cache
 
     all_forms = list(RegistryForm.objects.all())
     if isinstance(instance, RegistryForm) and instance not in all_forms:
